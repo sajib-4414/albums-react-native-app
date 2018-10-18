001a0ce67c980de927ad3d8a621830663f06f46d
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2018-10-18 11:55:22 +0600

    header file text loads with formatting in index js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /** @format */
 //Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
-
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 //Create a component
 /*
 A component is a javascript function which returns some amount of JSX
  */
 const App = () => (
-      <Text>Some Text</Text>
+      <Header />
  );
 
 
---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+//Import libraries for making a component
+import React from 'react';
+import { Text } from 'react-native';
+
+//Make a component
+
+const Header = () => {
+    const { textStyle } = styles;
+    return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+    textStyle: {
+        fontSize: 20,
+
+    }
+};
+
+
+//Make the component available to the other parts of the app
+export default Header;
